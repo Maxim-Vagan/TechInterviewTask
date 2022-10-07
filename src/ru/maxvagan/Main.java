6d33f6dece84585c618f009b16d705e0ad11b557
Maxim_Vagan <maxut@gmail.com>
2022-10-08 00:07:39 +0300

    PR: Практического задания Билета № 5 Технического собеседования

---
+++ src/ru/maxvagan/Main.java
@@ -0,0 +1,17 @@
+package ru.maxvagan;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Student stud1 = new Student(20, "Maksim");
+        Student stud2 = new Student(36, "Maksim");
+        Student student = new Student(20, "Maksim");
+
+        System.out.println("HashCode of stud1 = " + stud1.hashCode());
+        System.out.println("HashCode of stud2 = " + stud2.hashCode());
+        System.out.println("Is stud1 equals stud2? > " + stud1.equals(stud2));
+        System.out.println("Is stud1 equals student? > " + stud1.equals(student));
+        System.out.println("Is stud2 equals \"Maksim\"? > " + stud2.equals("Maksim"));
+    }
+}
---
+++ src/ru/maxvagan/Student.java
@@ -0,0 +1,36 @@
+package ru.maxvagan;
+
+public class Student {
+    private int age;
+    private String name;
+
+    public int getAge(){
+        return this.age;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public Student(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    @Override
+    public int hashCode(){
+        return name.hashCode() + age;
+    }
+
+    @Override
+    public boolean equals(Object compObj){
+        if (compObj == this){
+            return true;
+        } else if (!(compObj instanceof Student) || this.hashCode() != compObj.hashCode()){
+            return false;
+        } else {
+            Student stud = (Student) compObj;
+            return (age == stud.getAge() && name.equals(stud.getName()));
+        }
+    }
+}
